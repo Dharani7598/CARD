7ae13479f1c7b80e4365ff975cf0e578cad9be86
Dharani7598 <148205502+Dharani7598@users.noreply.github.com>
2024-12-13 11:54:44 +0530

    Bootstrap_bl_card

---
+++ ReactBootstrap.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const ReactBootstrap = () => {
+  return (
+    <div> 
+<div className="card w-25">
+  <div className="card-body">
+  <img src="IMAGES/bl.jpg" className="card-img-top h-15 w-15" alt="..." />
+  <h6 className="card-title">BRUCE LEE</h6>
+  <h6 className="card-subtitle mb-6 text-body-secondary">Be Water,My Friend</h6>
+  <p className="card-text">If you spend too much time thinking about a thing, you’ll never get it done. Make at least one definite move daily toward your goal.</p>
+  <ul class="list-group list-group-flush">
+  <div class="card-header">
+   <h5> YOU ONLY HAVE 3 OPTION</h5>
+  </div>
+    <li class="list-group-item">1. DO AND DIE</li>
+    <li class="list-group-item">2. DO OR DIE</li>
+    <li class="list-group-item">3. DO THEN DIE</li>
+  </ul>
+  <a href="#" class="btn btn-success">DO</a> 
+  
+  <a href="#" class="btn btn-danger">DIE</a>
+  </div>
+</div>
+
+    </div>
+    )
+    }
+
+export default ReactBootstrap
